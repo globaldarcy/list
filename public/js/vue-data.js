3eec8f46f3b570de95f845e8dad682687eb8e0c1
Darcy <you@example.com>
2024-02-29 17:25:07 +0800

    update

---
+++ public/js/vue-data.js
@@ -0,0 +1,35 @@
+
+var goods = [
+    {
+        name: 'iPhone 12',
+        choose: 0,
+    },
+    {
+        name: '荣耀X30',
+        choose: 1,
+    },
+    {
+        name: '小米 11',
+        choose: 3,
+    },
+    {
+        name: '魅族 15',
+        choose: 0,
+    },
+    {
+        name: '华为P30',
+        choose: 0,
+    },
+    {
+        name: 'OPPO R15',
+        choose: 0,
+    },
+    {
+        name: 'vivo NEX 3',
+        choose: 0,
+    },
+    {
+        name: 'HUAWEI Mate 30',
+        choose: 0,
+    },
+];
